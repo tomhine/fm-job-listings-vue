09f812017e25367d9600749059197d16251d4ff8
Tom Hine <tomhine792@gmail.com>
2022-03-29 16:30:34 +0100

    add default colors and sizings to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        spartan: ["Spartan", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          cyan: "hsl(180, 29%, 50%)",
+        },
+        neutral: {
+          bg: "hsl(180, 52%, 96%)",
+          lightcyan: "hsl(180, 31%, 95%)",
+          cyan: "hsl(180, 8%, 52%)",
+          darkcyan: "hsl(180, 14%, 20%)",
+        },
+      },
+      spacing: {
+        22: "88px",
+        39: "156px",
+      },
+      backgroundImage: {
+        "header-desktop": "url('/bg-header-desktop.svg')",
+        "header-mobile": "url('/bg-header-mobile.svg')",
+      },
+    },
   },
   plugins: [],
 };

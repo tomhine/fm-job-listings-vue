0bc4b9f914c01f67441cdd169f7eea6bc8c3ff7d
Tom Hine <tomhine792@gmail.com>
2022-03-29 16:33:14 +0100

    add component for job list transition

---
+++ src/transitions/JobListTransition.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+  <TransitionGroup name="list">
+    <slot></slot>
+  </TransitionGroup>
+</template>
+
+<style>
+.list-enter-active {
+  transition: all 0.2s ease-in;
+}
+.list-leave-active {
+  transition: all 0.2s ease-out;
+  position: absolute;
+}
+.list-enter-from {
+  transform: translateX(-100px);
+  opacity: 0;
+}
+.list-leave-to {
+  transform: translateX(100px);
+  opacity: 0;
+}
+
+.list-move {
+  transition: all 0.2s ease;
+}
+</style>

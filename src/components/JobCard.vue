de405eb46dad1e00182998c8073f1f29e4d34159
Tom Hine <tomhine792@gmail.com>
2022-03-29 16:32:53 +0100

    card component added

---
+++ src/components/JobCard.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { useJobsStore } from "@/stores/jobs";
+import type { Job } from "@/data";
+
+const { job } = defineProps<{ job: Job }>();
+const jobs = useJobsStore();
+</script>
+
+<template>
+  <article
+    class="lg:border-l-6 relative flex w-full flex-col gap-4 rounded border-l-4 bg-white px-6 pt-8 pb-6 lg:static lg:flex-row lg:justify-between"
+    :class="{ 'border-primary-cyan': job.featured, 'border-white': !job.featured }"
+  >
+    <div class="flex gap-8">
+      <img
+        :src="job.logo"
+        :alt="`${job.company} logo`"
+        class="absolute -top-6 left-6 h-12 w-12 lg:static lg:h-22 lg:w-22"
+      />
+      <div class="flex flex-col gap-2">
+        <div class="flex items-center gap-4">
+          <h3 class="text-sm font-bold text-primary-cyan">{{ job.company }}</h3>
+          <div class="flex items-center gap-2">
+            <span
+              v-if="job.new"
+              class="flex items-center rounded-full bg-primary-cyan py-1 px-2 text-[10px] font-bold uppercase text-white"
+              >new!</span
+            >
+            <span
+              v-if="job.featured"
+              class="flex items-center rounded-full bg-black py-1 px-2 text-[10px] font-bold uppercase text-white"
+              >featured</span
+            >
+          </div>
+        </div>
+        <h2 class="font-bold text-neutral-darkcyan">{{ job.position }}</h2>
+        <ul class="flex list-disc items-center gap-6 text-sm text-neutral-cyan">
+          <li class="list-none">{{ job.postedAt }}</li>
+          <li>{{ job.contract }}</li>
+          <li>{{ job.location }}</li>
+        </ul>
+      </div>
+    </div>
+    <div class="h-[1px] w-full bg-neutral-cyan/60 lg:hidden"></div>
+    <ul class="flex flex-wrap items-center gap-4">
+      <li
+        v-for="tag in [job.role, job.level, ...job.languages, ...job.tools]"
+        :key="tag"
+        class="cursor-pointer rounded bg-neutral-lightcyan px-2 py-1 text-sm font-bold text-primary-cyan"
+        @click="jobs.addFilter(tag)"
+      >
+        {{ tag }}
+      </li>
+    </ul>
+  </article>
+</template>

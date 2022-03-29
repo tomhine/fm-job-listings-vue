7153505fc09cde6a3e575bfec46d1cdc29bbc278
Tom Hine <tomhine792@gmail.com>
2022-03-29 16:32:00 +0100

    header added for background svg images

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header
+    class="h-39 w-full bg-primary-cyan bg-header-mobile lg:bg-header-desktop lg:bg-right"
+  ></header>
+</template>
+
+<style></style>
